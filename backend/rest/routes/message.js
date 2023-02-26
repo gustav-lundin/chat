1fcc323b651c506d8bb3e9d5e464ffcf33c6ee47
lunndin <70664281+lunndin@users.noreply.github.com>
2023-02-27 00:23:40 +0100

    message delete route

--- backend/rest/routes/message.js
+++ backend/rest/routes/message.js
@@ -1,15 +1,30 @@
 const messageRouter = require("express").Router();
+const AppError = require("../../apperror.js");
 const { Message } = require("../../models/index.js");
 const { tryCatch } = require("../../util/trycatch");
-// const authorizeChatRequest = require("../../acl/chat-acl.js");
-
-// messageRouter.all("*", authorizeChatRequest("messages"));
 
 messageRouter.post(
   "/",
   tryCatch(async (req, res) => {
     const message = await Message.create(req.body);
     res.json(message.toJSON());
+  })
+);
+
+messageRouter.delete(
+  "/:messageId",
+  tryCatch(async (req, res) => {
+    const messageId = req.params.messageId;
+    if (!messageId) {
+      throw new AppError("No message id provided", 404);
+    }
+    const message = await Message.findByPk(messageId);
+    if (!message) {
+      throw new AppError("Message not found", 404);
+    } else {
+      await message.destroy();
+      res.json({ status: "ok" });
+    }
   })
 );
 
