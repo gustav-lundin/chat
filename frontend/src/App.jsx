2426488fe1103adc239a231b8f9cd15887f5fd8c
lunndin <70664281+lunndin@users.noreply.github.com>
2023-03-05 21:12:44 +0100

    setup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Chats from "./pages/chats";
+import Chat from "./pages/chat";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="card">
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/chats" element={<Chats />} />
+      <Route path="/chat" element={<Chat />} />
+    </Routes>
   );
 }
 
